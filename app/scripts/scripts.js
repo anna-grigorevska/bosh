b49a8f079bbda91c6b2f96d286803e48034f0444
Vladislav Lypivets <avasl333@gmail.com>
2019-11-09 22:22:50 +0200

    add anumation for hide/show section

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,4 +1,13 @@
 "use strict";
 'use strict';
 
-console.log('here');
+$(document).ready(function () {
+  $('.technics input').on('change', function (e) {
+    $('.technics input').prop("disabled", true);
+    $('.section-toggle.open').removeClass('open');
+    setTimeout(function () {
+      $('.' + e.target.id).addClass('open');
+      $('.technics input').prop("disabled", false);
+    }, 900);
+  });
+});
--- scripts/domain/main.js
+++ scripts/domain/main.js
@@ -1,1 +1,12 @@
-console.log('here')
+$(document).ready(function() {
+  $('.technics input').on('change', function (e) {
+    $('.technics input').prop( "disabled", true );
+    $('.section-toggle.open').removeClass('open');
+    setTimeout(
+      () => {
+        $(`.${e.target.id}`).addClass('open');
+        $('.technics input').prop( "disabled", false );
+      }
+      , 900)
+  })
+})
